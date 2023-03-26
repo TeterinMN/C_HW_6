a183c6f0e1923aeb789656de1ba8e233859355f5
TeterinMN <Scorpion.TMN@ya.ru>
2023-03-26 09:13:09 +0700

    Finish task 41, new task 43

--- Task_41/Program.cs
+++ Task_41/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿Console.Clear();
+Console.Write("Введите количество чисел: ");
+    int count = int.Parse(Console.ReadLine()!);
+    int positiveCount = 0;
+    for (int i = 0; i < count; i++) {
+        Console.Write($"Введите число {i + 1}: ");
+        int number = int.Parse(Console.ReadLine()!);
+        if (number > 0) {
+            positiveCount++;
+        }
+    }
+
+Console.WriteLine($"Количество положительных чисел: {positiveCount}");
